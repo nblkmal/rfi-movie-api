09298da270b661701a20e4be64aff37a5d835789
nblkmal <nblkmal@gmail.com>
2022-01-26 18:21:56 +0800

    add factory

---
+++ database/factories/GenreFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Generator as Faker;
+
+class GenreFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+        ];
+    }
+    
+}
