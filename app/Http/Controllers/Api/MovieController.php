58e41a5eeec6084d3ae48fb0fcd5ef9e6c46023f
nblkmal <nblkmal@gmail.com>
2022-01-27 16:46:59 +0800

    update getGenre & getTimeSlot function

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MovieController extends Controller
+{
+    public function getGenre(Request $request)
+    {
+        $movies = Movie::with('genre')->get();
+
+        $genre = $request->genre;
+
+        if($genre)
+        {
+            $movies = Movie::with('genre', 'theater', 'language')->whereHas('genre', function($query) use($genre) {
+                $query->where('name', $genre);
+                return $query;
+            })->get();
+        }
+
+        return response()->json([
+            'name' => 'Genre',
+            'status' => true,
+            'message' => 'Sucessfully fetch movies with genre '.$request->genre,
+            'data' => $movies,
+        ], 200);
+    }
+
+    public function getTimeSlot(Request $request)
+    {
+        $movies = Movie::with('genre', 'theater', 'language');
+
+        $theater_name = $request->theater_name;
+        $time_start = $request->time_start;
+        $time_end = $request->time_end;
+
+        if($theater_name)
+        {
+            $movies->whereHas('theater', function($query) use($theater_name) {
+                $query->where('name', $theater_name);
+                return $query;
+            });
+        }
+
+        if($time_start)
+        {
+            $movies->where('time_start', $time_start);
+        }
+
+        if($time_end)
+        {
+            $movies->where('time_end', $time_end);
+        }
+
+        return response()->json([
+            'name' => 'TimeSlot',
+            'status' => true,
+            'message' => 'Sucessfully fetch movies with genre '.$request->genre,
+            'data' => $movies->get(),
+        ], 200);
+    }
+}
