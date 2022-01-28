60a811f635f7814a052b1c214e99c93254deb832
nblkmal <nblkmal@gmail.com>
2022-01-28 11:42:48 +0800

    performer : add relationship movies

--- app/Models/Performer.php
+++ app/Models/Performer.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Movie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Performer extends Model
 {
@@ -12,4 +13,9 @@ class Performer extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function movies()
+    {
+        return $this->belongsToMany(Movie::class, 'movie_performers');
+    }
 }
