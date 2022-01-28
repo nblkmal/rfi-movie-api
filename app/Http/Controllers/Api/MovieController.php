519a1deb9bd8028e8100c0452467d37b77f17a6f
nblkmal <nblkmal@gmail.com>
2022-01-28 12:37:25 +0800

    add post rating function

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Rate;
+use App\Models\User;
 use App\Models\Movie;
+use App\Models\Performer;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\RatingRequest;
 
 class MovieController extends Controller
 {
@@ -94,29 +98,71 @@ class MovieController extends Controller
 
     public function getSearchPerformer(Request $request)
     {
-        $movies = Movie::with('genre', 'theater', 'language');
+        $performer = Performer::with('movies');
 
-        $theater_name = $request->theater_name;
-        $d_date = $request->d_date;
+        $performer_name = $request->performer_name;
 
-        if($theater_name)
+        if($performer_name)
         {
-            $movies->whereHas('theater', function($query) use($theater_name) {
-                $query->where('name', $theater_name);
-                return $query;
-            });
-        }
-
-        if($d_date)
-        {
-            $movies->where('d_date', $d_date);
+            $performer->where('name','LIKE','%'.$performer_name.'%');
         }
 
         return response()->json([
             'name' => 'Search Performer',
             'status' => true,
             'message' => 'Sucessfully fetch movies',
-            'data' => $movies->get(),
+            'data' => $performer->get(),
+        ], 200);
+    }
+
+    public function postRating(RatingRequest $request)
+    {
+        $movie = $request->movie_title;
+        $user = $request->username;
+
+        if($movie)
+        {
+            $movie_id = Movie::where('title','LIKE','%'.$movie.'%')->first()->id;
+        }
+
+        if($user)
+        {
+            $user_id = User::where('name','LIKE','%'.$user.'%')->first()->id;
+        }
+
+        $rate = Rate::create([
+            'value' => $request->rating,
+            'description' => $request->r_description,
+            'movie_id' => $movie_id,
+            'user_id' => $user_id,
+        ]);
+
+        return response()->json([
+            'name' => 'Give Rating',
+            'status' => true,
+            'message' => 'Sucessfully submit rating',
+            'data' => $rate,
+        ], 200);
+    }
+
+    public function getNewMovies()
+    {
+        
+        return response()->json([
+            'name' => 'New Movies',
+            'status' => true,
+            'message' => 'Sucessfully fetch new movies',
+            'data' => $performer->get(),
+        ], 200);
+    }
+
+    public function postMovie()
+    {
+        return response()->json([
+            'name' => 'Add movie',
+            'status' => true,
+            'message' => 'Sucessfully add new movie',
+            'data' => $performer->get(),
         ], 200);
     }
 }
