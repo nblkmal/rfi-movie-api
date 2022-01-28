b1f0db29873d972f06c84e197d08b8915e531793
nblkmal <nblkmal@gmail.com>
2022-01-28 16:58:28 +0800

    add custom MovieRequest

---
+++ app/Http/Requests/Api/MovieRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class MovieRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'release' => 'required',
+            'length' => 'required',
+            'description' => 'required',
+            'mpaa_rating' => 'required',
+            'genre' => 'required',
+            'director' => 'required',
+            'performer' => 'required',
+            'language' => 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
